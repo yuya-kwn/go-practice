d441b5ce063cfb5c427bf3a477055e330e37a304
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2022-11-10 17:54:05 +0900

    APIサーバを立てる

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello,World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+	fmt.Println("サーバー起動")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
 }
