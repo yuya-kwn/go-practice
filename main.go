45f2a4e7ce7387b7ca9bd4bfbe8c0b50b3f15ad3
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2022-11-10 22:25:07 +0900

    ステータスコード

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
+			w.WriteHeader(http.StatusOK)
 			fmt.Fprintf(w, "GET!")
 		case http.MethodPost:
 			fmt.Fprintf(w, "POST!")
+			w.WriteHeader(http.StatusOK)
 		default:
 			fmt.Fprintf(w, "Method not Allowed")
+			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
 	})
 
